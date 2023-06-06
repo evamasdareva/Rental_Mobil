8dbe4741a7b8302ccdcc41ac50d1f6dd69074144
evamasdareva <evamasdareva@gmail.com>
2023-06-06 17:22:47 +0700

    praktikum8-12

---
+++ app/Models/Mobil.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mobil extends Model
+{
+    use HasFactory;
+
+    protected $table = "mobils";
+
+    protected $fillable = [
+        'nama_mobil',
+        'cc',
+        'tipe_mobil_id',
+        'tahun_mobil',
+        'nomor_polisi',
+        'warna',
+        'merk_id',
+        'foto',
+    ];
+}
